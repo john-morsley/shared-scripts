0e812fc54fd17673b8911bf71df8a519be83ad55
John Morsley <johnmorsley@gmail.com>
2020-10-25 21:40:27 +0000

    Corrected spacing mistake

--- k3s/install_k3s.sh
+++ k3s/install_k3s.sh
@@ -31,7 +31,7 @@ K3S_OPTIONS="INSTALL_K3S_VERSION=${INSTALL_K3S_VERSION}"
 
 {
   echo "Download the K3s install script and execute it..."
-  COMMAND = "curl -sfL ${K3S_SCRIPT_URL} | ${K3S_OPTIONS} sh -"
+  COMMAND="curl -sfL ${K3S_SCRIPT_URL} | ${K3S_OPTIONS} sh -"
   $($COMMAND)
   echo "KubeConfig:"
   echo "export kubeconfig=$(sudo cat /etc/rancher/k3s/k3s.yaml)"
